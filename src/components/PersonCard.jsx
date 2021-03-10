bcc95347aff7a7f09a3a90e03899fccd56ec35e2
Becca Theismann <becca@theismannconsulting.com>
2021-03-10 14:53:33 -0500

    All Working

--- src/components/PersonCard.jsx
+++ src/components/PersonCard.jsx
@@ -3,10 +3,14 @@ import React , { Component } from 'react';
 class PersonCard extends Component {
     render(){
         return (
-            <div>
-                <h1> {this.props.lastName} {this.props.firstName} </h1>
-                <p> Age: {this.props.age} </p>
-                <p> Hair Color: {this.props.hairColor} </p>
+            <div className="d-flex flex-wrap justify-content-center align-items-center align-content-center">
+                <div class="card w-25">
+                    <div class="card border-info mb-3">
+                        <h1 class="card-title, text-left"> {this.props.lastName} {this.props.firstName} </h1>
+                        <p class="card-text, text-left"> Age: {this.props.age} </p>
+                        <p class="card-text, text-left"> Hair Color: {this.props.hairColor} </p>
+                    </div>
+                </div>
             </div>
         );
     }
